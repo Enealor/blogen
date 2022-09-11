b5e2b75fd3578c59852b6036ed0ce4d4393c85b4
eleanor.holland <eleanor.fractal@gmail.com>
2022-09-11 16:09:11 -0700

    ch3 complete
    Refactored to use an "Internal" (and potentially unstable/unsafe).
    This also expose a "safe" subset.

---
+++ Html/Internal.hs
@@ -0,0 +1,97 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+module Html.Internal where
+
+-- * Types
+
+-- | Html type representing a complete html document
+newtype Html = Html String
+
+-- | Html type representing structures such as headings and paragraphs to
+--   be embedded into an html document.
+newtype Structure = Structure String
+
+-- | Html type alias for Title.
+type Title = String
+
+-- * EDSL
+
+-- | Converts title and an html structure into an html document.
+--   The html structure is used as the body of the document.
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    (el "html"
+      ( el "head" (el "title" (escape title))
+      <> el "body" (getStructureString content)
+      )
+    )
+
+-- | Wraps content in a p tag.
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+-- | Wraps content in a h1 tag.
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
+
+-- | Wraps content in a pre tag.
+code_ :: String -> Structure
+code_ = Structure . el "pre" . escape
+
+-- | Converts a list of structures into an unordered list structure.
+ul_ :: [Structure] -> Structure
+ul_ structureList = Structure (el "ul" (getStructureString (toListStructure structureList)))
+
+-- | Converts a list of structures into an ordered list structure.
+ol_ :: [Structure] -> Structure
+ol_ structureList = Structure (el "ol" (getStructureString (toListStructure structureList)))
+
+-- | Combines structure a and structure b into a single structure.
+--   In effect, this is string concatination.
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+-- * Render
+
+-- | Returns html content as a string.
+render :: Html -> String
+render (Html a) = a
+
+-- * Utilities
+
+-- | Gets string content from a structure.
+getStructureString :: Structure -> String
+getStructureString (Structure a) = a
+
+-- | Wraps content in the provided tag.
+--   e.g. tag "foo" "bar" = "<foo>bar</foo>"
+el :: String -> String -> String
+el tagName content = "<" <> tagName <> ">" <> content <> "</" <> tagName <> ">"
+
+-- | Replaces html specific characters with "safe" versions.
+escape :: String -> String
+escape =
+  let
+    escapeChar c =
+      case c of
+        '<'  -> "&lt;"
+        '>'  -> "&gt;"
+        '&'  -> "&amp;"
+        '"'  -> "&quot;"
+        '\'' -> "&#39;"
+        _    -> [c]
+  in
+    concatMap escapeChar
+
+-- | Wraps a structure for use in a list.
+listItemWrap :: Structure -> Structure
+listItemWrap (Structure content) = Structure (el "li" content)
+
+-- | Converts a list of structures into a single structure. Each structure has the <li> tag added.
+toListStructure :: [Structure] -> Structure
+toListStructure = Structure . concatMap (el "li" . getStructureString)
+
+-- -- Initial recursive approach
+-- toListStructure (i:is) = append_ (listItemWrap i) (toListStructure is)
+-- toListStructure [] = Structure ""
+
--- hello.hs
+++ hello.hs
@@ -4,15 +4,27 @@ main :: IO ()
 main = putStrLn (render myhtml)
 
 myhtml :: Html
-myhtml = html_
-  "<Hello Title!>"
-  (append_
-    (h1_ "<tag>&Hello, world!</tag>")
+myhtml = html_ "Hello Title!" body
+
+body :: Structure
+body = append_
+    (append_ 
+      (h1_ "Hello, world!")
+      (ol_ [p_ "first", p_ "before three and after one", p_ "what?"])
+    )
     (append_
-      (p_ "\"Learning html and haskell.\"")
+      (p_ "Learning html and haskell.")
       (append_
         (p_ "Coding this with the help of lhbg-book.")
-        (p_ "\'Making this content into a mess.\'")
+        (append_
+          (p_ "Making this content into a mess.")
+          (append_
+            (code_ htmlCode)
+            (ul_ [p_ "apple", p_ "banana", p_ "watermelon", p_ "tomato"])
+          )
+        )
       )
     )
-  )
+
+htmlCode :: String
+htmlCode = render (html_ "What is this?" (code_ ".&<><><>&.This is a test.&<><><>&."))
